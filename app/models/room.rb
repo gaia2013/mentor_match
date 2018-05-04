e69453108308a06c1576e9ddb6d8c5a728365000
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-05-05 06:57:49 +0900

    モデル間のリレーション設定 room_user user room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
+  has_many :users, through: :room_users
+  has_many :room_users
+
   validates :name, presence: true, length:{maximum: 50}
   validates :content, presence: true, length:{ maximum: 500}
+
+  accepts_nested_attributes_for :room_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :rooms, through: :room_users
+  has_many :room_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
