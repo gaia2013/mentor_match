1b25cee13e858a10da055f11ce590265eaf96079
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-05-05 16:02:42 +0900

    users/#/showに返答履歴表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @comments = @user.comments
+    room_users_ids = @user.room_users.pluck(:id)
+    @comments = Comment.where(room_user_id: room_users_ids)
+
   end
 end
